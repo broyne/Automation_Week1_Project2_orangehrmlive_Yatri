bb43b565c8c8eec7e19869823782fec0ed865fa0
yatrihirani <hiraniyatri@gmail.com>
2024-05-21 23:11:15 +1000

    Automation_Week1_Project2_Yatri home work done

---
+++ src/orangehrmlive/ChromeBrowser.java
@@ -0,0 +1,59 @@
+package orangehrmlive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeBrowser {
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    public static void main(String[] args) {
+
+        //1. Setup Chrome Browser
+        WebDriver driver = new ChromeDriver();
+
+        //2. Open the URL into browser
+        driver.get(baseUrl);
+
+        //Maximise the browser window
+        driver.manage().window().maximize();
+
+        //We give Implicit wait to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //3. Print the title of the page
+        System.out.println("Main Title of the page is: " +driver.getTitle());
+
+        //4. Print the current url.
+        System.out.println("Current URL is: " +driver.getCurrentUrl());
+
+        //5. Print the page source.
+        System.out.println("This the Page source: " +driver.getPageSource());
+
+        //6. Click on ‘Forgot your password?’ link.
+        driver.findElement(By.className("orangehrm-login-forgot-header")).click();
+
+        //7. Print the current URL.
+        System.out.println("Current URL is: "+driver.getCurrentUrl());
+
+        //8. Navigate back to the login page.
+        driver.navigate().back();
+
+        //9. Refresh the page.
+        driver.navigate().refresh();
+
+        //10. Enter the email to email field.
+        WebElement emailField = driver.findElement(By.name("username"));
+        emailField.sendKeys("Admin");
+
+        //11. Enter Password to the password field
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("admin123");
+
+        //12. close the browser
+        driver.quit();
+    }
+}
